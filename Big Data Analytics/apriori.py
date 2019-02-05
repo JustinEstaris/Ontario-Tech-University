f6ddca037b2c5f5f807f6d265407ba5412b4ad4d
Justin Estaris <justin.estaris@uoit.net>
2019-02-05 13:54:43 -0500

    Added Big Data Analytics Files

---
+++ Big Data Analytics/apriori.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from collections import Counter
+
+
+file = open("sample.dat", "r")
+
+#Create new empty array
+data = []
+
+# =============================================================================
+# file_list = open("sample.dat").read().splitlines()
+# print(file_list)
+# =============================================================================
+
+#Loop through the file line by line
+for row in file:
+
+
+
+#How the Apriori Works
+    #1. Reads Data
+    #2. Counts each element's frequency
+    #3. Removes any element from the list that doesn't meet the Support Threshold
+    #4. Creates 2-element combinations
+    
+#Steps for the Apriori
+    # Create an empty list
+    # Create a for-loop that iterates each line 
+        #Read File Line by Line
+        #Convert each Line into a List
+        #Append each List to the empty list
+        
+# =============================================================================
+# 
+#     #Store Rows - [List Data Type]
+#     basket = row.split()
+#     #keys = Counter(basket).keys()
+#     frequency = Counter(basket)
+#     #print (keys)
+#     #print(row)
+#     #print (frequency)
+# =============================================================================
